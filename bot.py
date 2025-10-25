c1a1e133645caafac80831175a69944693ce8585
maxknez <41018858+maxknez@users.noreply.github.com>
2025-10-26 00:14:18 +0300

    0.1

--- bot.py
+++ bot.py
@@ -3,11 +3,12 @@ import discord
 from discord import app_commands
 from dotenv import load_dotenv
 
-# Загружаем токен из .env
+# Загружаем переменные окружения
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
-# Настраиваем клиента
+# Настройка клиента
 intents = discord.Intents.default()
 bot = discord.Client(intents=intents)
 tree = app_commands.CommandTree(bot)
@@ -18,7 +19,14 @@ async def on_ready():
     await tree.sync()
     print(f"Бот {bot.user} запущен и готов к работе")
 
-# Простая команда /пинг
+    # Отправляем сообщение в указанный канал
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send("я жив, сучки!")
+    else:
+        print("❗ Канал не найден. Проверь CHANNEL_ID в .env")
+
+# Команда /пинг
 @tree.command(name="пинг", description="Проверить задержку бота")
 async def ping_command(interaction: discord.Interaction):
     latency_ms = round(bot.latency * 1000)
